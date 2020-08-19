c2e531c7a0991eee0ea3cc2b54b534fcc7558761
jboro <joseph@videomyjob.com>
2020-08-19 13:36:26 +0800

    - added Polly for retrying connection in saving and fetching hives

--- BCTApp/BCTApp/Services/FirebaseHelper.cs
+++ BCTApp/BCTApp/Services/FirebaseHelper.cs
@@ -1,9 +1,13 @@
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using BCTApp.Models;
 using Firebase.Database;
 using Firebase.Database.Query;
+using Polly;
 
 namespace BCTApp
 {
@@ -13,20 +17,46 @@ namespace BCTApp
         
         public async Task AddHive(string userId, Hive hive)
         {
-            await firebase.Child($"BeeHives/{userId}")
-                .PostAsync(new Hive()
+             await Policy.Handle<HttpRequestException>(ex =>
                 {
-                    HiveName = hive.HiveName,
-                    HiveLocation = hive.HiveLocation
-                });
+                    Debug.WriteLine($"{ex.Message}");
+                    return true;
+                })
+                .WaitAndRetryAsync(
+                    retryCount: 5,
+                    sleepDurationProvider: retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)),
+                    onRetry: (ex, time) =>
+                    {
+                        Console.WriteLine($"Something went wrong: {ex.Message}, retrying...");
+                    }
+                ).ExecuteAsync(async () =>  await firebase.Child($"BeeHives/{userId}")
+                    .PostAsync(new Hive()
+                    {
+                        HiveName = hive.HiveName,
+                        HiveLocation = hive.HiveLocation
+                    }));
+
+           
         }
         
         
         public async Task<List<Hive>> GetAllUserHives(string userId)
         {
-            var hives = 
-                await firebase.Child($"BeeHives/{userId}")
-                    .OnceAsync<Hive>();
+            var hives = await Policy.Handle<HttpRequestException>(ex =>
+                {
+                    Debug.WriteLine($"{ex.Message}");
+                    return true;
+                })
+                .WaitAndRetryAsync(
+                    retryCount: 5,
+                    sleepDurationProvider: retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)),
+                    onRetry: (ex, time) =>
+                    {
+                        Console.WriteLine($"Something went wrong: {ex.Message}, retrying...");
+                    }
+                ).ExecuteAsync(async () => await firebase.Child($"BeeHives/{userId}")
+                    .OnceAsync<Hive>());
+          
             
             return hives.Select(x=> new Hive()
             {
